eb0acacc8cd13ca077835f1bfca5f9d46fe86a6d
Adão Raul <adao.raul@gmail.com>
2025-07-29 19:48:07 -0300

    Fix Windows build error with template dependent name lookup
    - Add explicit 'this->' prefix to access base class members in template
    - Required by C++ standard for dependent name lookup in templates
    - Fixes compilation error on Windows with MSVC

--- src/node_printer_win.cc
+++ src/node_printer_win.cc
@@ -26,7 +26,7 @@ namespace{
         * @param iSizeKbytes size in bytes of required allocating memory
         */
         MemValue(const DWORD iSizeKbytes) {
-            _value = (Type*)malloc(iSizeKbytes);
+            this->_value = (Type*)malloc(iSizeKbytes);
         }
 		
         ~MemValue () {
@@ -34,10 +34,10 @@ namespace{
         }
     protected:
         virtual void free() {
-            if(_value != NULL)
+            if(this->_value != NULL)
             {
-                ::free(_value);
-                _value = NULL;
+                ::free(this->_value);
+                this->_value = NULL;
             }
         }
     };
